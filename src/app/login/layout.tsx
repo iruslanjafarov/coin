538dd43b2c10da3bf0c04d31cd50559ac54ff2f0
Ruslan Jafarov <rusyajaf@gmail.com>
2025-06-03 11:22:45 +0300

    feat(login): add login page with layout, remove account button from login

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,23 @@
+import type { FC, ReactNode } from 'react';
+
+import '@/styles/style.css';
+
+interface ILoginLayout {
+	children: Readonly<ReactNode>;
+}
+
+/**
+ * LoginLayout component used to wrap the login page.
+ *
+ * This layout component simply returns the passed children without any additional layout structure.
+ * Useful for isolating the login route from the main application layout (e.g., without a header or sidebar).
+ *
+ * @param children - The React nodes to be rendered within the layout.
+ * @returns The rendered children.
+ */
+
+const LoginLayout: FC<ILoginLayout> = ({ children }) => {
+	return children;
+};
+
+export default LoginLayout;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,19 @@
+import Container from '@/components/container';
+
+/**
+ * Login page component.
+ *
+ * This component renders the login page, including a form with email and password fields.
+ *
+ * @returns A layout container with the login form elements.
+ */
+
+const Login = () => {
+	return (
+		<Container className='flex justify-center items-center w-full h-full'>
+			<h1>hello, this is login page</h1>
+		</Container>
+	);
+};
+
+export default Login;
