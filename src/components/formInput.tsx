5e9a43f3bc7ac285cb1bcbba3fb324835c86798b
Ruslan Jafarov <rusyajaf@gmail.com>
2025-06-03 12:19:08 +0300

    feat(form): add form component

---
+++ src/components/formInput.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import {
+	FieldValues,
+	Path,
+	UseFormRegister,
+	FieldError,
+} from 'react-hook-form';
+
+interface FormInputProps<T extends FieldValues> {
+	type: string;
+	name: Path<T>;
+	label?: string;
+	placeholder?: string;
+	register: UseFormRegister<T>;
+	required?: boolean;
+	error?: FieldError;
+}
+
+/**
+ * Reusable input component integrated with react-hook-form.
+ *
+ * @param type - Input type (e.g., "email", "password").
+ * @param name - Name of the field, matches key in form values.
+ * @param label - Optional field label.
+ * @param placeholder - Optional placeholder text.
+ * @param register - react-hook-form's register function.
+ * @param required - Whether the field is required.
+ * @param error - Optional error object for validation state.
+ * @returns Controlled input connected to react-hook-form.
+ */
+
+const FormInput = <T extends FieldValues>({
+	type,
+	name,
+	label,
+	placeholder,
+	register,
+	required = false,
+	error,
+}: FormInputProps<T>) => {
+	return (
+		<div className='flex flex-col space-y-1'>
+			{label && (
+				<label className={error ? 'border-red-500' : ''} htmlFor={name}>
+					{label}
+				</label>
+			)}
+			<input
+				type={type}
+				id={name}
+				placeholder={placeholder}
+				{...register(name, { required })}
+				className={`border px-3 py-2 rounded-md ${
+					error ? 'border-red-500' : ''
+				}`}
+			/>
+			{error && <span className='text-sm text-red-500'>{error.message}</span>}
+		</div>
+	);
+};
+
+export default FormInput;
