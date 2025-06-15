652540ae433991a816056cb86dcb75ca6fe45054
Ruslan Jafarov <rusyajaf@gmail.com>
2025-06-15 15:10:20 +0300

    feat(avatar): add dropdown meny

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import Container from './container';
+import Avatar from './avatar';
 
 import Icon from '@/assets/icon.svg';
 
@@ -17,9 +18,8 @@ import Icon from '@/assets/icon.svg';
 const Header = () => {
 	const pathname: string = usePathname();
 
-	const hideAccountButtonRoutes: string[] = ['/login', '/account'];
-	const hideAccountButtonCondition: boolean =
-		hideAccountButtonRoutes.includes(pathname);
+	const hideAccountRoutes: string[] = ['/login'];
+	const hideAccountCondition: boolean = hideAccountRoutes.includes(pathname);
 
 	return (
 		<header>
@@ -32,14 +32,7 @@ const Header = () => {
 						priority={true}
 					/>
 				</Link>
-				{!hideAccountButtonCondition && (
-					<Link
-						href='/account'
-						className='bg-[#FFD700]/70 px-3 py-2 rounded-lg flex items-center gap-2 transition-transform duration-200 hover:-translate-y-0.5 cursor-pointer'
-					>
-						Личный кабинет
-					</Link>
-				)}
+				{!hideAccountCondition && <Avatar />}
 			</Container>
 		</header>
 	);
