a12f2e6483f2981f941f79a162e3dbdecc4b6a1c
Ruslan Jafarov <rusyajaf@gmail.com>
2025-06-19 21:11:30 +0300

    feat(auth): add auth state to global store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,11 +3,13 @@ import { IItem } from '@/types/item';
 import { create } from 'zustand';
 
 interface IStore {
+	isAuth: boolean;
 	items: IItem[];
 	item: IItem | null;
 	itemId: string | null;
 	setItemWithId: (id: string, item: IItem) => void;
 	itemHistory: number[];
+	setIsAuth: (authState: boolean) => void;
 	setItem: (item: IItem) => void;
 	setItems: (items: IItem[]) => void;
 	clearItem: () => void;
@@ -16,30 +18,33 @@ interface IStore {
 }
 
 /**
- * Zustand store for managing a list of items (e.g. cryptocurrencies),
- * as well as a selected item for detailed view, and its price history.
+ * Zustand store `useStore` that manages the state related to items (e.g., cryptocurrencies) and user authentication.
  *
- * Store state includes:
- * - `items`: the full list of cryptocurrency items.
- * - `item`: the currently selected item (used on detail pages).
- * - `itemHistory`: an array of past price values for the selected item, used for rendering charts.
+ * Contains:
+ * - `isAuth`: a boolean flag indicating if the user is authenticated.
+ * - `items`: a list of all available items.
+ * - `item`: the currently selected item (e.g., for a detailed view).
+ * - `itemId`: the ID of the currently selected item.
+ * - `itemHistory`: an array containing the price history of the selected item, capped at 10 values (used for charts).
  *
- * Store actions include:
- * - `setItems`: sets the full list of items.
- * - `setItem`: sets the currently selected item and resets `itemHistory`.
- * - `clearItem`: clears the selected item and price history.
- * - `randomizePrices`: simulates live price changes for all items in the list.
- * - `randomizeItemPrice`: simulates price fluctuation for the selected item and appends to `itemHistory`.
- *
- * @returns Zustand store with managed state and update functions for crypto UI behavior.
+ * Actions:
+ * - `setIsAuth(authState)`: sets the authentication state.
+ * - `setItems(items)`: sets the full list of items.
+ * - `setItem(item)`: sets the currently selected item.
+ * - `setItemWithId(id, item)`: sets both the item and its ID.
+ * - `clearItem()`: clears the selected item, its ID, and its price history.
+ * - `randomizePrices()`: simulates random price changes across all items (±5), useful for mocking live updates.
+ * - `randomizeItemPrice()`: simulates a price fluctuation for the selected item and appends the new price to its history.
  */
 
 const useStore = create<IStore>((set, get) => ({
+	isAuth: false,
 	items: [],
 	item: null,
 	itemId: null,
 	itemHistory: [],
 
+	setIsAuth: (authState) => set({ isAuth: authState }),
 	setItem: (item) => set({ item }),
 	setItems: (items) => set({ items }),
 	setItemWithId: (id, item) => set({ item, itemId: id }),
