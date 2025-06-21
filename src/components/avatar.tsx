73aba9f38a2dd34fba7f436e0c1f9cb873973815
Ruslan Jafarov <rusyajaf@gmail.com>
2025-06-21 09:00:37 +0300

    fix(top-loader): use router from lib

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'nextjs-toploader/app';
 import Image from 'next/image';
 
 import useStore from '@/store/store';
